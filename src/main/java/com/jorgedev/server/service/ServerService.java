c681b55b5f3ab7cd989265a7c587d524c7f771b8
jorgeLRM <jorge.lrm99@gmail.com>
2023-01-16 17:52:19 -0600

    Add server service implementation

--- src/main/java/com/jorgedev/server/service/ServerService.java
+++ src/main/java/com/jorgedev/server/service/ServerService.java
@@ -1,4 +1,17 @@
 package com.jorgedev.server.service;
 
+import com.jorgedev.server.model.Server;
+
+import java.io.IOException;
+import java.util.Collection;
+
 public interface ServerService {
+
+    Server create(Server server);
+    Server ping(String ipAddress) throws IOException;
+    Collection<Server> list(int limit);
+    Server get(long id);
+    Server update(Server server);
+    Boolean delete(long id);
+
 }
---
+++ src/main/java/com/jorgedev/server/service/implementation/ServerServiceImpl.java
@@ -0,0 +1,74 @@
+package com.jorgedev.server.service.implementation;
+
+import com.jorgedev.server.model.Server;
+import com.jorgedev.server.repository.ServerRepository;
+import com.jorgedev.server.service.ServerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Collection;
+
+import static com.jorgedev.server.enumeration.Status.SERVER_DOWN;
+import static com.jorgedev.server.enumeration.Status.SERVER_UP;
+import static java.lang.Boolean.TRUE;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
+public class ServerServiceImpl implements ServerService {
+
+    private final ServerRepository serverRepository;
+
+    @Override
+    public Server create(Server server) {
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepository.save(server);
+    }
+
+    @Override
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepository.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepository.save(server);
+        return server;
+    }
+
+    @Override
+    public Collection<Server> list(int limit) {
+        log.info("Fetching all servers");
+        return serverRepository.findAll(PageRequest.of(0, limit)).toList();
+    }
+
+    @Override
+    public Server get(long id) {
+        log.info("Fetching server by id: {}", id);
+        return serverRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Server update(Server server) {
+        log.info("Updating server: {}", server.getName());
+        return serverRepository.save(server);
+    }
+
+    @Override
+    public Boolean delete(long id) {
+        log.info("Deleting server by ID: {}", id);
+        serverRepository.deleteById(id);
+        return TRUE;
+    }
+
+    private String setServerImageUrl() {
+        return "";
+    }
+}
